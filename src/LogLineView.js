7cf0623d024c0eac52a6148ac0dc418b7a48d74e
EC2 Default User <ec2-user@ip-172-31-47-196.ap-northeast-1.compute.internal>
2019-09-04 15:22:27 +0000

    split code and improve performance

---
+++ src/LogLineView.js
@@ -0,0 +1,148 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx';
+import { fade, withStyles } from '@material-ui/core/styles';
+import ReactJson from 'react-json-view';
+
+const styles = (theme) => ({
+  root: {},
+  line: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: theme.spacing(0, 2, 0, 0),
+    '&:hover': {
+      background: 'rgba(0,0,0,0.1)',
+    },
+    '&$error': {
+      color: 'red',
+      fontWeight: 'bold',
+    },
+    '&$warn': {
+      color: '#ca9106',
+      fontWeight: 'bold',
+    },
+    '&$info': {
+      color: '#005eda',
+    },
+    '&$trace': {
+      color: '#616161',
+    },
+    '&$debug': {
+      color: 'inherit',
+    },
+    '&$log': {
+      color: '#616161',
+    },
+    '&$selected': {
+      background: 'rgba(255,255,0,0.3)',
+    },
+  },
+  moreInfoToggle: {
+    width: theme.spacing(2),
+    textAlign: 'center',
+    '&$notExists': {},
+    '&$exists': {
+      '&$open': {
+        '&::after': {
+          content: `"-"`,
+        }
+      },
+      '&$closed': {
+        '&::after': {
+          content: `"+"`,
+        }
+      }
+    }
+  },
+  component: {},
+  timestamp: {},
+  level: {},
+  text: {},
+  moreInfo: {
+    background: '#f5f5f588',
+    boxShadow: 'inset 0px 10px 16px -10px #0000001a, inset 0px -10px 16px -10px #0000001a',
+    padding: theme.spacing(2, 1),
+    fontSize: 14,
+    zoom: 0.9,
+  },
+  error: {},
+  warn: {},
+  info: {},
+  trace: {},
+  debug: {},
+  log: {},
+  selected: {},
+  exists: {},
+  notExists: {},
+  open: {},
+  closed: {},
+})
+
+class LogLineView extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = this._getInitialState()
+  }
+  
+  _getInitialState() {
+    return {
+      isMoreInfoOpen: false,
+    }
+  }
+  
+  _toggleMoreInfo(key) {
+    this.setState({ isMoreInfoOpen: !this.state.isMoreInfoOpen })
+  }
+  
+  render() {
+    const { classes, className: classNameProp, event, isSelected = false, ...props } = this.props
+    const { isMoreInfoOpen } = this.state
+    
+    const hasMoreInfo = event.exception || event.objects.length > 0
+    
+    return (
+      <div className={clsx(classes.root, classNameProp)} {...props}>
+        
+        <div className={clsx(classes.line, {
+          [classes.selected]: isSelected,
+          [classes.wrror]: event.level === 'ERROR',
+          [classes.warn]: event.level === 'WARN',
+          [classes.info]: event.level === 'INFO',
+          [classes.trace]: event.level === 'TRACE',
+          [classes.debug]: event.level === 'DEBUG',
+          [classes.log]: event.level === 'LOG',
+        })}>
+          <div className={clsx(classes.moreInfoToggle, {
+            [classes.exists]: hasMoreInfo,
+            [classes.notExists]: !hasMoreInfo,
+            [classes.open]: isMoreInfoOpen,
+            [classes.closed]: !isMoreInfoOpen,
+          })} onClick={() => hasMoreInfo && this._toggleMoreInfo()}>
+          </div>
+          <div style={{ display: 'inline' }}>
+            <span className={classes.timestamp}>{event.time}</span>&nbsp;
+            <span className={classes.component}>{event.component}</span>&nbsp;
+            <span className={classes.level}>{event.level}</span>&nbsp;
+            <span className={classes.text}>{event.text}</span>
+          </div>
+        </div>
+        
+        { hasMoreInfo && isMoreInfoOpen &&
+          <div className={clsx(classes.moreInfo)}>
+            <ReactJson src={event} name={false} displayObjectSize={false} displayDataTypes={false} />
+          </div>
+        }
+        
+      </div>
+    )
+  }
+}
+
+LogLineView.propTypes = {
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  event: PropTypes.object.isRequired,
+  isSelected: PropTypes.bool,
+}
+
+export default withStyles(styles)(LogLineView)
--- src/LogView.js
+++ src/LogView.js
@@ -16,6 +16,7 @@ import Button from '@material-ui/core/Button';
 import UnfoldLess from '@material-ui/icons/UnfoldLess';
 import UnfoldMore from '@material-ui/icons/UnfoldMore';
 import ReactJson from 'react-json-view';
+import LogLineView from './LogLineView';
 
 const styles = (theme) => ({
   root: {},
@@ -111,79 +112,6 @@ const styles = (theme) => ({
     flexDirection: 'column',
     width: '100%',
   },
-  line: {
-    display: 'flex',
-    flexDirection: 'row',
-    padding: theme.spacing(0, 2, 0, 0),
-    '&:hover': {
-      background: 'rgba(0,0,0,0.1)',
-    },
-    '&$error': {
-      color: 'red',
-      fontWeight: 'bold',
-    },
-    '&$warn': {
-      color: '#ca9106',
-      fontWeight: 'bold',
-    },
-    '&$info': {
-      color: '#005eda',
-    },
-    '&$trace': {
-      color: '#616161',
-    },
-    '&$debug': {
-      color: 'inherit',
-    },
-    '&$log': {
-      color: '#616161',
-    },
-    '&$selected': {
-      background: 'rgba(255,255,0,0.3)',
-    },
-  },
-  moreInfoToggle: {
-    width: theme.spacing(2),
-    textAlign: 'center',
-    '&$notExists': {},
-    '&$exists': {
-      '&$open': {
-        '&::after': {
-          content: `"-"`,
-        }
-      },
-      '&$closed': {
-        '&::after': {
-          content: `"+"`,
-        }
-      }
-    }
-  },
-  component: {},
-  timestamp: {},
-  level: {},
-  text: {},
-  moreInfo: {
-    background: '#f5f5f588',
-    boxShadow: 'inset 0px 10px 16px -10px #0000001a, inset 0px -10px 16px -10px #0000001a',
-    padding: theme.spacing(2, 1),
-    fontSize: 14,
-    zoom: 0.9,
-    '&$closed': {
-      display: 'none',
-    },
-  },
-  error: {},
-  warn: {},
-  info: {},
-  trace: {},
-  debug: {},
-  log: {},
-  selected: {},
-  exists: {},
-  notExists: {},
-  open: {},
-  closed: {},
 })
 
 const LogLevel = {
@@ -219,19 +147,6 @@ class LogView extends React.PureComponent {
   _handleChangeRegexFilter(event) {
     event.preventDefault()
     this.setState({ regexFilter: event.target.value })
-  }
-  
-  _toggleMoreInfo(key) {
-    const moreInfoOpen = this.state.moreInfoOpen.slice()
-    let idx = moreInfoOpen.indexOf(key)
-    
-    if (idx < 0) {
-      moreInfoOpen.push(key)
-    } else {
-      moreInfoOpen.splice(idx, 1)
-    }
-    
-    this.setState({ moreInfoOpen })
   }
   
   render() {
@@ -294,52 +209,21 @@ class LogView extends React.PureComponent {
           </div>
           <div className={classes.content}>
             <div className={classes.rows}>
-              {log.map((event) => {
-                let hasMoreInfo = event.exception || event.objects.length > 0
-                
+              { log.map((event) => {
                 if (LogLevel[event.level].value < LogLevel[levelFilter].value
                     || (re && !re.exec(`${event.time} ${event.level} ${event.text}`))) {
                   return null // ignore this line
                 } else {
                   return (
-                    <div id={`L${event._key}`} key={event._key} className={classes.row}>
-                      
-                      <div className={clsx(classes.line, {
-                        [classes.selected]: selected.includes(event._key),
-                        [classes.wrror]: event.level === 'ERROR',
-                        [classes.warn]: event.level === 'WARN',
-                        [classes.info]: event.level === 'INFO',
-                        [classes.trace]: event.level === 'TRACE',
-                        [classes.debug]: event.level === 'DEBUG',
-                        [classes.log]: event.level === 'LOG',
-                      })}>
-                        <div className={clsx(classes.moreInfoToggle, {
-                          [classes.exists]: hasMoreInfo,
-                          [classes.notExists]: !hasMoreInfo,
-                          [classes.open]: moreInfoOpen.includes(event._key),
-                          [classes.closed]: !moreInfoOpen.includes(event._key),
-                        })} onClick={() => hasMoreInfo && this._toggleMoreInfo(event._key)}>
-                        </div>
-                        <div style={{ display: 'inline' }}>
-                          <span className={classes.timestamp}>{event.time}</span>&nbsp;
-                          <span className={classes.component}>{event.component}</span>&nbsp;
-                          <span className={classes.level}>{event.level}</span>&nbsp;
-                          <span className={classes.text}>{event.text}</span>
-                        </div>
-                      </div>
-                      
-                      { hasMoreInfo &&
-                        <div className={clsx(classes.moreInfo, {
-                          [classes.closed]: !moreInfoOpen.includes(event._key)
-                        })}>
-                          <ReactJson src={event} name={false} displayObjectSize={false} displayDataTypes={false} />
-                        </div>
-                      }
-                      
-                    </div>
+                    <LogLineView 
+                      id={`L${event._key}`} 
+                      key={event._key}
+                      className={classes.row}
+                      event={event}
+                      isSelected={selected.includes(event._key)} />
                   )
                 }
-              })}
+              }) }
             </div>
           </div>
         </Paper>
